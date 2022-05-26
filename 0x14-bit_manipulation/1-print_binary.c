6dcd4e84ec69011fd9d612fe9439c45b9a66d39d
MulukenSholaye <muluken@alx.com>
2022-05-26 10:39:33 -0700

    task1

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -5,8 +5,9 @@
  */
 void print_binary(unsigned long int n)
 {
-if (n > 1)
-print_binary(n >> 1);
+unsigned long int tt = n;
+if (tt > 1)
+print_binary(tt >> 1);
 
-_putchar((n & 1) + '0');
+_putchar((tt & 1) + '0');
 }
