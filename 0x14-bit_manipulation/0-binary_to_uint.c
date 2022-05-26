6e485e430d5c8e8b4d1ca9499dfac2e81a900d15
MulukenSholaye <muluken@alx.com>
2022-05-26 10:32:42 -0700

    0 task

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -8,15 +8,15 @@
 
 unsigned int binary_to_uint(const char *b)
 {
-unsigned int az, vec = 0;
+unsigned int azi, vec = 0;
 
 if (!b)
 return (0);
-for (az = 0; b[az] != '\0'; az++)
+for (azi = 0; b[azi] != '\0'; azi++)
 {
-if (b[az] != '0' && b[az] != '1')
+if (b[azi] != '0' && b[azi] != '1')
 return (0);
-if (b[a] == '1')
+if (b[azi] == '1')
 vec = vec * 2 + 1;
 else
 vec = vec * 2 + 0;
